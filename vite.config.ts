a9d82058a44ab78ae85bec052044e85cb741c383
andressagonsalvespereira <andressagonsalvespereira@gmail.com>
2025-04-01 15:56:46 -0300

    fix: SPA routing and Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,25 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(), // ativa só no dev
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
     },
   },
 }));
